6f58020411bc40a00964103acc59407970241631
Gaurav-Mahato <81543021+Gaurav-Mahato@users.noreply.github.com>
2021-07-08 08:28:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+var i;
+for (i = 0; i < 7; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function() {
+    makeSound(this.innerHTML);
+    makeAnimation(this.innerHTML);
+
+  });
+}
+document.addEventListener("keydown", function(event) {
+  btn = event.key;
+  makeSound(btn);
+  makeAnimation(btn);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio1 = new Audio("sounds/tom-1.mp3");
+      audio1.play();
+      break;
+    case "a":
+      var audio2 = new Audio("sounds/tom-2.mp3");
+      audio2.play();
+      break;
+    case "s":
+      var audio3 = new Audio("sounds/tom-3.mp3");
+      audio3.play();
+      break;
+    case "d":
+      var audio4 = new Audio("sounds/tom-4.mp3");
+      audio4.play();
+      break;
+    case "j":
+      var audio5 = new Audio("sounds/snare.mp3");
+      audio5.play();
+      break;
+    case "k":
+      var audio6 = new Audio("sounds/crash.mp3");
+      audio6.play();
+      break;
+    case "l":
+      var audio7 = new Audio("sounds/kick-bass.mp3");
+      audio7.play();
+      break;
+  }
+}
+function makeAnimation(key){
+  document.querySelector("."+key).classList.add("pressed");
+  setTimeout(function(){
+    document.querySelector("."+key).classList.remove("pressed");
+  },100);
+}
